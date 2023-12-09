60247a6cddc095206cabd8321fe86b8eae5e5e48
chukwudibarrah <118562380+chukwudibarrah@users.noreply.github.com>
2023-12-09 19:00:07 +0000

    test

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: '/wudiandweber/',
+        path: '/',
         element: <Home />,
       },
       {
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()
   ],
-  base: '/wudiandweber/',
   optimizeDeps: {
     include: ['react', 'react-dom'],
   },
